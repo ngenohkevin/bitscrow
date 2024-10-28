d3ee234a002a99e18126ee0229f430e0e2a89fa1
ngenohkevin <ngenohkevin19@gmail.com>
2024-10-28 10:31:30 +0300

    sqlc data models

---
+++ db/query/escrow_transaction.sql
@@ -0,0 +1,12 @@
+-- name: CreateEscrowTransactions :one
+INSERT INTO escrow_transactions (id, buyer_id, seller_id, bitcoin_address, amount, status, created_at)
+VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;
+
+-- name: GetEscrowTransactions :one
+SELECT * FROM escrow_transactions WHERE id = $1;
+
+-- name: UpdateEscrowTransactions :exec
+UPDATE escrow_transactions
+SET buyer_id = $2, seller_id = $3, bitcoin_address = $4, amount = $5, status = $6, created_at = $7
+WHERE id = $1
+RETURNING *;
