fcd66eea86a350a49ce68125f71749e7d0a64eaa
ngenohkevin <ngenohkevin19@gmail.com>
2024-11-02 12:39:27 +0300

    test cases

--- db/query/escrow_transaction.sql
+++ db/query/escrow_transaction.sql
@@ -1,12 +1,12 @@
 -- name: CreateEscrowTransactions :one
-INSERT INTO escrow_transactions (id, buyer_id, seller_id, bitcoin_address, amount, status, created_at)
-VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;
+INSERT INTO escrow_transactions (id, buyer_id, seller_id, bitcoin_address, amount, status)
+VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;
 
 -- name: GetEscrowTransactions :one
 SELECT * FROM escrow_transactions WHERE id = $1;
 
 -- name: UpdateEscrowTransactions :exec
 UPDATE escrow_transactions
-SET buyer_id = $2, seller_id = $3, bitcoin_address = $4, amount = $5, status = $6, created_at = $7
+SET buyer_id = $2, seller_id = $3, bitcoin_address = $4, amount = $5, status = $6
 WHERE id = $1
 RETURNING *;
