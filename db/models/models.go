8e8614e6f0c666933feaa16514f2f476f1ab315b
ngenohkevin <ngenohkevin19@gmail.com>
2024-11-08 12:26:51 +0300

    change the timestamp in createdAt from pgtype to timestamptz

--- db/models/models.go
+++ db/models/models.go
@@ -5,26 +5,28 @@
 package db
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type Dispute struct {
-	ID         uuid.UUID        `json:"id"`
-	EscrowID   pgtype.UUID      `json:"escrow_id"`
-	Reason     string           `json:"reason"`
-	Resolved   pgtype.Bool      `json:"resolved"`
-	Resolution pgtype.Text      `json:"resolution"`
-	CreatedAt  pgtype.Timestamp `json:"created_at"`
-	UpdatedAt  pgtype.Timestamp `json:"updated_at"`
+	ID         uuid.UUID          `json:"id"`
+	EscrowID   pgtype.UUID        `json:"escrow_id"`
+	Reason     string             `json:"reason"`
+	Resolved   pgtype.Bool        `json:"resolved"`
+	Resolution pgtype.Text        `json:"resolution"`
+	CreatedAt  time.Time          `json:"created_at"`
+	UpdatedAt  pgtype.Timestamptz `json:"updated_at"`
 }
 
 type EscrowTransaction struct {
-	ID             uuid.UUID        `json:"id"`
-	BuyerID        uuid.UUID        `json:"buyer_id"`
-	SellerID       uuid.UUID        `json:"seller_id"`
-	BitcoinAddress string           `json:"bitcoin_address"`
-	Amount         pgtype.Numeric   `json:"amount"`
-	Status         pgtype.Text      `json:"status"`
-	CreatedAt      pgtype.Timestamp `json:"created_at"`
+	ID             uuid.UUID      `json:"id"`
+	BuyerID        uuid.UUID      `json:"buyer_id"`
+	SellerID       uuid.UUID      `json:"seller_id"`
+	BitcoinAddress string         `json:"bitcoin_address"`
+	Amount         pgtype.Numeric `json:"amount"`
+	Status         pgtype.Text    `json:"status"`
+	CreatedAt      time.Time      `json:"created_at"`
 }
