9525e249058ab2d46e49ad8239fe72a8954e3538
ngenohkevin <ngenohkevin19@gmail.com>
2024-11-08 11:55:56 +0300

    added uuid in the database

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/ngenohkevin/bitscrow/config"
+	"log"
+)
 
 func main() {
-	fmt.Println("The escrow project")
+	configurations, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
+
+	connPool, err := pgxpool.New(context.Background(), configurations.DbUrl)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	runDBMigrations(configurations.MigrationURL, configurations.DbUrl)
+
+	log.Println("Connected to the database")
+
+	defer connPool.Close()
+}
+
+func runDBMigrations(migrationURL string, dbURL string) {
+	migration, err := migrate.New(migrationURL, dbURL)
+	if err != nil {
+		log.Fatal("cannot create migration:", err)
+	}
+	if err = migration.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		log.Fatal("failed to run migration up:", err)
+	}
+	log.Println("migration successful")
 }
