4992e036a7a455b2452602dc8874b16d3d24a8b4
ngenohkevin <ngenohkevin19@gmail.com>
2024-11-08 11:56:18 +0300

    added uuid in the database

---
+++ internal/store/store.go
@@ -0,0 +1,25 @@
+package store
+
+import (
+	"github.com/jackc/pgx/v5/pgxpool"
+	db "github.com/ngenohkevin/bitscrow/db/models"
+)
+
+// Store defines all functions to execute db queries and transactions
+type Store interface {
+	db.Querier
+}
+
+// SQLStore provides all functions to execute db queries and transactions
+type SQLStore struct {
+	connPool *pgxpool.Pool
+	*db.Queries
+}
+
+// NewStore creates a new store
+func NewStore(connPool *pgxpool.Pool) Store {
+	return &SQLStore{
+		connPool: connPool,
+		Queries:  db.New(connPool),
+	}
+}
