576e45ca7b74dbf66d25d6fe2b2839d8c7958d80
ngenohkevin <ngenohkevin19@gmail.com>
2024-11-14 10:49:53 +0300

    Test: escrow update escrow

--- db/query/escrow_transaction.sql
+++ db/query/escrow_transaction.sql
@@ -5,7 +5,7 @@ VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;
 -- name: GetEscrowTransactions :one
 SELECT * FROM escrow_transactions WHERE id = $1;
 
--- name: UpdateEscrowTransactions :exec
+-- name: UpdateEscrowTransactions :one
 UPDATE escrow_transactions
 SET buyer_id = $2, seller_id = $3, bitcoin_address = $4, amount = $5, status = $6
 WHERE id = $1
